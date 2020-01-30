d501f7893a8a628bbb8bfdba4d457889ab6df68b
Andrey Sobolev <haiodo@gmail.com>
2020-01-30 22:09:49 +0700

    Move to metrics segments
    Signed-off-by: Andrey Sobolev <haiodo@gmail.com>

--- pkg/api/registry/gen.go
+++ pkg/api/registry/gen.go
@@ -17,4 +17,4 @@
 // Package registry provides the
 package registry
 
-//go:generate bash -c "protoc -I . registry.proto --go_out=plugins=grpc:. --proto_path=$GOPATH/src/ --proto_path=$GOPATH/pkg/mod/  --proto_path=$( go list -f '{{ .Dir }}' -m github.com/golang/protobuf )"
+//go:generate bash -c "protoc -I . registry.proto --go_out=plugins=grpc,paths=source_relative:. --proto_path=$GOPATH/src/ --proto_path=$GOPATH/pkg/mod/  --proto_path=$( go list -f '{{ .Dir }}' -m github.com/golang/protobuf )"
