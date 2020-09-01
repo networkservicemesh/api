52a6c6071aa6bf23c60cc5dfda188a00c827ad8b
Vladimir Popov <vladimir.popov@xored.com>
2020-09-01 15:21:52 +0700

    Revert changes for the kernel mechanism
    Signed-off-by: Vladimir Popov <vladimir.popov@xored.com>

--- pkg/api/networkservice/mechanisms/kernel/constants.go
+++ pkg/api/networkservice/mechanisms/kernel/constants.go
@@ -30,6 +30,9 @@ const (
 	// LinuxIfMaxLength - Linux has a limit of 15 characters for an interface name
 	LinuxIfMaxLength = 15
 
+	// PCIAddress string
+	PCIAddress = "PCIAddress"
+
 	// NetNSInodeKey - netns inode mechanism property key
 	NetNSInodeKey = common.NetNSInodeKey
 
